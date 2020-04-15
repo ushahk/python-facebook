77dd2e3272ad82c1a0dd8a1d5be8fdd10259842f
Naga Pavan Konda <nkonda@Padmas-MacBook-Air.local>
2020-04-14 20:56:00 -0700

    create palindrome python script

---
+++ create-palindrome.py
@@ -0,0 +1,55 @@
+def palindrome_center(c):
+    p={}
+    if len(c)%2!=0:
+        for i in range (1,len(c)-1):
+            for j in range(1,len(c)):
+                if i-j>-1 and i+j<len(c) and c[i-j]==c[i+j]:
+                    p[i]=j
+                else:
+                    break
+    else:
+        for i in range (1,len(c)-1):
+            for j in range(1,len(c)):
+                if i-j>-1 and i+j<len(c) and c[i-j]==c[-(i-j+1)]:
+                    p[i]=j
+                else:
+                    break
+    print(p)
+    return p
+
+
+def marker_palindrome(x):
+    meta_dict=palindrome_center(x)
+    ld=len(meta_dict)
+    if ld%2!=0:
+        center=int(ld/2)+1
+    else:
+        center=ld/2
+    pc=-1
+    meta_keys=list(meta_dict.keys())
+    for i,j in enumerate(meta_keys):
+        if i>0 and meta_keys[i]-meta_keys[i-1]==1:
+            pc=i
+        else:
+            continue
+    print(pc)
+    return pc
+
+
+def create_palindrome(x):
+    mark=marker_palindrome(x)
+    palindrome=''
+    for i in range(mark+1,len(x)):
+        palindrome=x[i]+x
+        continue
+    return palindrome
+
+def frange(sta,sto,jmp):
+    v=[]
+    while sta<sto:
+        v.append(sta)
+        sta+=jmp
+    return v
+
+
+create_palindrome(input("enter string: "))
