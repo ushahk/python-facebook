d6dbe70cd56138e8ba0f16ed471ca01174248fcf
Naga Pavan Konda <nkonda@Padmas-MacBook-Air.local>
2020-04-23 21:35:58 -0700

    Dinosaur final

--- dinosaur.py
+++ dinosaur.py
@@ -1,4 +1,4 @@
-import csv
+import csv,math
 dino={}
 with open("dino1.csv") as dino1:
   csv_reader=csv.reader(dino1,delimiter=",")
@@ -17,6 +17,17 @@ with open("dino2.csv") as dino2:
       else:
           dino[row[0]]=""
           dino[row[0]]=[row[1],row[2]]
+
+def speed(v):
+    return ((float(v[2]) / float(v[0])) - 1) * (math.sqrt(float(v[0]) * 9.8))
+print(dino.pop('NAME'))
+dino_speed={}
+ds=[]
 for i,v in dino.items():
     if len(v)>2:
-        print(f'{i} : {v}')
+        #print(f'{i} : {v}')
+        dino_speed[speed(v)]=i
+        ds.append(speed(v))
+    elif len(v)<=2:
+        print(i)
+print(dino_speed[(sorted(ds,reverse=1)[0])])
